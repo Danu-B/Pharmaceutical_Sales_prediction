7096515ed4d895074cc33b227611f57794700627
unknown <dbulom12@gmail.com>
2022-06-03 22:07:21 +0300

    Unittest

---
+++ scripts/unit_test.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import unittest
+import pandas as pd
+
+sys.path.append(os.path.abspath(os.path.join('../scripts')))
+from file_handler import FileHandler
+
+
+class Unit_Test(unittest.TestCase):
+
+    def setUp(self):
+        self.helper = FileHandler()
+
+    def test_to_csv(self):
+        df = pd.DataFrame({'unit': range(1,4), 'test': range(3,6)})
+        self.helper.to_csv(df, '../data/tests.csv', False)
+        df2 = pd.read_csv('../data/tests.csv')
+        self.assertEqual(df.shape, df2.shape)
+
+    def test_read_csv(self):
+        df = self.helper.read_csv('../data/tests.csv')
+        df2 = pd.read_csv('../data/tests.csv')
+        self.assertEqual(df.shape, df2.shape)
+
+if __name__ == '__main__':
+    unittest.main()
